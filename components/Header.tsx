307275e3de2fa4d6f9918f0dadd805d7cf654d4d
Arslaan Qadus <azy2k9@gmail.com>
2021-10-14 22:11:21 +0100

    Implemented header and chakra setup

---
+++ components/Header.tsx
@@ -0,0 +1,130 @@
+import React, { useRef } from 'react';
+
+import { Flex, Heading, Link } from '@chakra-ui/layout';
+import {
+    IconButton,
+    useColorMode,
+    useDisclosure,
+    useBreakpointValue,
+    Drawer,
+    DrawerOverlay,
+    DrawerHeader,
+    DrawerBody,
+    DrawerContent,
+    DrawerCloseButton,
+} from '@chakra-ui/react';
+import {
+    BsTwitter,
+    BsLinkedin,
+    BsGithub,
+    BsMoonStarsFill,
+    BsSunFill,
+} from 'react-icons/bs';
+import { BiMenu } from 'react-icons/bi';
+import { CgClose } from 'react-icons/cg';
+import ButtonLink from './ButtonLink';
+
+const Header = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const size = useBreakpointValue({ base: 'full', md: 'sm' });
+
+    return (
+        <Flex justify="space-between" pt="8">
+            <Flex w="xs">
+                <Link target="_blank" href="https://twitter.com/reactdevjnr">
+                    <IconButton aria-label="Twitter" variant="ghost" ml="4">
+                        <BsTwitter />
+                    </IconButton>
+                </Link>
+                <Link
+                    target="_blank"
+                    href="https://www.linkedin.com/in/arslaan-qadus/"
+                >
+                    <IconButton aria-label="Linkedin" variant="ghost" mx="1">
+                        <BsLinkedin />
+                    </IconButton>
+                </Link>
+                <Link target="_blank" href="https://github.com/azy2k9">
+                    <IconButton aria-label="Github" variant="ghost" mx="1">
+                        <BsGithub />
+                    </IconButton>
+                </Link>
+            </Flex>
+            <Flex w="md" justify="center">
+                <Heading size="lg">Arslaan Qadus</Heading>
+            </Flex>
+            <Flex w="xs" justify="flex-end" mr="4">
+                <IconButton
+                    aria-label="Github"
+                    variant="ghost"
+                    onClick={toggleColorMode}
+                >
+                    {colorMode === 'light' ? (
+                        <BsMoonStarsFill />
+                    ) : (
+                        <BsSunFill />
+                    )}
+                </IconButton>
+                <IconButton aria-label="menu" variant="ghost" onClick={onOpen}>
+                    {isOpen ? <CgClose /> : <BiMenu />}
+                </IconButton>
+                <Drawer
+                    isOpen={isOpen}
+                    placement="right"
+                    onClose={onClose}
+                    size={size}
+                >
+                    <DrawerOverlay />
+                    <DrawerContent>
+                        <DrawerCloseButton />
+                        <DrawerHeader />
+                        <DrawerBody>
+                            <Flex
+                                justifyContent="center"
+                                py={{ base: 2 }}
+                                flexDir="column"
+                                height="100%"
+                                alignItems="normal"
+                            >
+                                <ButtonLink variant="ghost" href="/" py="20">
+                                    Home
+                                </ButtonLink>
+                                <ButtonLink
+                                    variant="ghost"
+                                    href="/projects"
+                                    py="20"
+                                >
+                                    Projects
+                                </ButtonLink>
+                                <ButtonLink
+                                    variant="ghost"
+                                    href="/blog"
+                                    py="20"
+                                >
+                                    Blog
+                                </ButtonLink>
+                                <ButtonLink
+                                    variant="ghost"
+                                    href="/about"
+                                    py="20"
+                                >
+                                    About Me
+                                </ButtonLink>
+                                <ButtonLink
+                                    variant="ghost"
+                                    href="/contact"
+                                    py="20"
+                                >
+                                    Contact
+                                </ButtonLink>
+                            </Flex>
+                        </DrawerBody>
+                    </DrawerContent>
+                </Drawer>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default Header;
---
+++ theme.ts
@@ -0,0 +1,35 @@
+import { ThemeConfig } from '@chakra-ui/theme';
+
+const config: ThemeConfig = {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+};
+const Theme = {
+    colors: {
+        primary: '#200065',
+        text: {},
+    },
+    config,
+    fonts: {
+        body: 'Roboto, sans-serif',
+        heading: 'Georgia, serif',
+        mono: 'Menlo, monospace',
+    },
+    fontSizes: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        md: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '3.75rem',
+        '7xl': '4.5rem',
+        '8xl': '6rem',
+        '9xl': '8rem',
+    },
+};
+
+export { Theme };
