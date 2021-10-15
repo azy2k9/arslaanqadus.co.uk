5f81be9a881ac6bcc5af53acd385286423e639e0
Arslaan Qadus <azy2k9@gmail.com>
2021-10-15 22:15:22 +0100

    Blog section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,17 @@
-import { Heading, SimpleGrid, useBreakpointValue } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import BlogPost from '../components/BlogPost';
-import BlogPostList from '../components/BlogPostList';
 import Header from '../components/Header';
-import FeaturedBlog from '../components/FeaturedBlog';
+import Blog from '../sections/Blog';
 
 const Home: NextPage = () => {
-    const headingSize = useBreakpointValue({ base: 'md', md: 'lg' });
     return (
         <>
-            <div>
-                <Head>
-                    <title>Homepage</title>
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-            </div>
+            <Head>
+                <title>Home</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Header />
-            <FeaturedBlog />
-            <Heading textAlign="center" size={headingSize}>
-                Some other blog posts...
-            </Heading>
-            <BlogPostList />
+            <Blog />
         </>
     );
 };
---
+++ sections/Blog.tsx
@@ -0,0 +1,33 @@
+import { Heading, useBreakpointValue } from '@chakra-ui/react';
+import React from 'react';
+import BlogPostList from '../components/BlogPostList';
+import FeaturedBlog from '../components/FeaturedBlog';
+
+const Blog = () => {
+    const headingSize = useBreakpointValue({ base: 'md', md: 'lg' });
+
+    return (
+        <>
+            <FeaturedBlog
+                title="What is React?"
+                previewText="React does not attempt to provide a complete 'application library'. It is designed specifically for building user interfaces and therefore does not include many of the tools some developers might consider necessary to build an application. This allows the choice of whichever libraries the developer prefers to accomplish tasks such as performing network access or local data storage. Common patterns of usage have emerged as the library matures."
+                direction="right"
+                tags={[{ tag: 'React', colorScheme: 'pink' }]}
+            />
+            <FeaturedBlog
+                title="JavaScript and Web Browsers"
+                previewText="Web browsers are capable of running JavaScript outside the sandbox, with the privileges necessary to, for example, create or delete files. Such privileges are not intended to be granted to code from the Web. Incorrectly granting privileges to JavaScript from the Web has played a role in vulnerabilities in both Internet Explorer"
+                tags={[
+                    { tag: 'New', colorScheme: 'green' },
+                    { tag: 'Featured', colorScheme: 'cyan' },
+                ]}
+            />
+            <Heading textAlign="center" size={headingSize}>
+                Some other blog posts...
+            </Heading>
+            <BlogPostList />
+        </>
+    );
+};
+
+export default Blog;
