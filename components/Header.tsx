8761d52f187976e432a742156b155d15271e47f7
Arslaan Qadus <azy2k9@gmail.com>
2021-10-17 01:33:59 +0100

    Footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 import { Flex, Heading, Link } from '@chakra-ui/layout';
 import {
--- components/IconButtonLink.tsx
+++ components/IconButtonLink.tsx
@@ -1,22 +1,25 @@
 import {
-	IconButton as ChakraIconButton,
-	IconButtonProps as ChakraIconButtonProps,
-} from "@chakra-ui/button";
-import NextLink from "next/link";
-import React from "react";
+    IconButton as ChakraIconButton,
+    IconButtonProps as ChakraIconButtonProps,
+} from '@chakra-ui/button';
+import { LinkProps as ChakraLinkProps } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import React from 'react';
 
 interface IIconButtonLink {
-	href: string;
+    href: string;
 }
 
-const IconButtonLink = (props: IIconButtonLink & ChakraIconButtonProps) => {
-	const { children, href, ...rest } = props;
+const IconButtonLink = (
+    props: IIconButtonLink & ChakraIconButtonProps & ChakraLinkProps
+) => {
+    const { children, href, ...rest } = props;
 
-	return (
-		<NextLink href={href} passHref>
-			<ChakraIconButton {...rest}>{children}</ChakraIconButton>
-		</NextLink>
-	);
+    return (
+        <NextLink href={href} passHref>
+            <ChakraIconButton {...rest}>{children}</ChakraIconButton>
+        </NextLink>
+    );
 };
 
 export default IconButtonLink;
