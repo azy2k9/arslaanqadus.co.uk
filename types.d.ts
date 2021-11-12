2f5474ca0a0da689c4bb8d6b3811f81592fb1394
Arslaan Qadus <azy2k9@gmail.com>
2021-11-12 17:18:06 +0000

    Implemented TypeScript based GraphQL codegen

--- types.d.ts
+++ types.d.ts
@@ -1,52 +1,52 @@
-interface BlogPost {
-    __typename: string,
-    id: string,
-    title: string,
-    slug: string,
-    introduction: string,
-    featured: boolean,
-    variant: "blog",
-    readTime: string,    
-    author: string,
-    content: {
-        markdown: string
-    },
-    thumbnail: {
-        id: string,
-        url: string
-    },
-    tags: [{
-        id: string,
-        value: string,
-        colorScheme: ThemeTypings['colorSchemes']
-    }],
-    createdAt: string
-}
+// interface BlogPost {
+//     __typename: string,
+//     id: string,
+//     title: string,
+//     slug: string,
+//     introduction: string,
+//     featured: boolean,
+//     variant: "blog",
+//     readTime: string,    
+//     author: string,
+//     content: {
+//         markdown: string
+//     },
+//     thumbnail: {
+//         id: string,
+//         url: string
+//     },
+//     tags: [{
+//         id: string,
+//         value: string,
+//         colorScheme: ThemeTypings['colorSchemes']
+//     }],
+//     createdAt: string
+// }
 
-type ProjectPost = Omit<BlogPost, "variant"> & { variant: "project" };
+// type ProjectPost = Omit<BlogPost, "variant"> & { variant: "project" };
 
-type PartialBlogPost = Pick<BlogPost, "id" | "title" | "introduction" | "featured" | "tags" | "slug" | "thumbnail" | "variant">
+// type PartialBlogPost = Pick<BlogPost, "id" | "title" | "introduction" | "featured" | "tags" | "slug" | "thumbnail" | "variant">
 
-type PartialProjectPost = Pick<ProjectPost, "id" | "title" | "introduction" | "featured" | "tags" | "slug" | "thumbnail" | "variant">
+// type PartialProjectPost = Pick<ProjectPost, "id" | "title" | "introduction" | "featured" | "tags" | "slug" | "thumbnail" | "variant">
 
-interface ThemeTypings {
-    colorSchemes:
-    | "whiteAlpha"
-    | "blackAlpha"
-    | "gray"
-    | "red"
-    | "orange"
-    | "yellow"
-    | "green"
-    | "teal"
-    | "blue"
-    | "cyan"
-    | "purple"
-    | "pink"
-    | "linkedin"
-    | "facebook"
-    | "messenger"
-    | "whatsapp"
-    | "twitter"
-    | "telegram"
-}
+// interface ThemeTypings {
+//     colorSchemes:
+//     | "whiteAlpha"
+//     | "blackAlpha"
+//     | "gray"
+//     | "red"
+//     | "orange"
+//     | "yellow"
+//     | "green"
+//     | "teal"
+//     | "blue"
+//     | "cyan"
+//     | "purple"
+//     | "pink"
+//     | "linkedin"
+//     | "facebook"
+//     | "messenger"
+//     | "whatsapp"
+//     | "twitter"
+//     | "telegram"
+// }
