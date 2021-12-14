6e5fe7c86a6e0f584fd7c3b7c5cfa3e370871711
Arslaan Qadus <azy2k9@gmail.com>
2021-12-14 17:36:48 +0000

    Implemented polyfill for smooth scrolling on iOS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../css/index.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import smoothscroll from 'smoothscroll-polyfill';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {        
+        // Polyfill the smooth scrolling behavior on iOS
+        smoothscroll.polyfill();
+    }, []);
+    
     return (
         <ChakraProvider>
             <Component {...pageProps} />
