929438f91ab4ce7df7ac0e158ad75b0520e15660
Arslaan Qadus <azy2k9@gmail.com>
2021-12-14 20:40:08 +0000

    Re enabled smooth scroll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../css/index.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-// import smoothscroll from 'smoothscroll-polyfill';
-// import { useEffect } from 'react';
+import smoothscroll from 'smoothscroll-polyfill';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    // useEffect(() => {        
-    //     // Polyfill the smooth scrolling behavior on iOS
-    //     smoothscroll.polyfill();
-    // }, []);
+    useEffect(() => {        
+        // Polyfill the smooth scrolling behavior on iOS
+        smoothscroll.polyfill();
+    }, []);
     
     return (
         <ChakraProvider>
