3df23a91efbdfc401cd3661e46400f78739acbb8
Arslaan Qadus <azy2k9@gmail.com>
2022-04-19 20:07:34 +0100

    Added vercel analytics to domain next js config

--- next.config.js
+++ next.config.js
@@ -16,6 +16,6 @@ module.exports = {
     ignoreDuringBuilds: true
   },
   images: {
-    domains: ['picsum.photos', 'media.graphcms.com', 'media.graphassets.com'],
+    domains: ['picsum.photos', 'media.graphcms.com', 'media.graphassets.com', 'vitals.vercel-insights.com'],
   },
 };
