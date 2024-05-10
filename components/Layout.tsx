7803b529046c277d8a5008cc5418ecf2ed563eca
azy2k9 <azy2k9@gmail.com>
2024-05-10 23:18:48 +0100

    Improve responsive styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -55,7 +55,7 @@ const Layout = ({
                 style={{ height: '100%', overflowY: 'scroll' }}
             >
                 <Header />
-                <Container minW={{ lg: 'container.lg' }}>
+                <Container minW={{ md: 'container.md', lg: 'container.lg' }}>
                     {enableHeading && (
                         <Heading textAlign="center" py={['8', '16']}>
                             {title}
